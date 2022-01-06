9042abe858297014de197273bfec3b4e9e9335e1
Danika H Jaskulak-Gonzalez <danikaherminise@gmail.com>
2022-01-05 18:04:14 -0800

    set up react router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 // imports
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import NavBar from './components/navbar/NavBar';
+import Home from './components/home/Home';
+import About from './components/about/About';
+import Market from './components/market/Market';
+import Create from './components/create/Create';
+import Error from "./components/Error";
 import LoginSetup from './LoginSetup';
 import Mint from './Mint';
 
-function App() {
+export default function App() {
   return (
     <div className='App'>
+      <NavBar />
+      <Routes>
+        <Route path="/" component={Home} exact />
+        <Route path="/about" component={About} />
+        <Route path="/market" component={Market} />
+        <Route path="/create" component={Create} />
+        <Route component={Error} />
+      </Routes>
       <LoginSetup />
       <Mint />
     </div>
   );
 }
-
-export default App;
---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,16 @@
+// imports 
+import React from "react";
+import {Link} from "react-router-dom";
+
+function Navbar() {
+  return (
+    <div>
+      <Link to="/"> Home </Link>
+      <Link to="/about"> About </Link>
+      <Link to="/market"> Market </Link>
+      <Link to="/create"> Create </Link>
+    </div>
+  );
+};
+
+export default Navbar;
