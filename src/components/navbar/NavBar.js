42060c78eaba6a3205c6436ba2a49d5253f9c87c
Danika H Jaskulak-Gonzalez <danikaherminise@gmail.com>
2022-01-06 17:49:20 -0800

    update

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,26 +1,23 @@
 // imports 
 import './NavBar.css';
 import React from "react";
-import { Navbar, Container } from 'react-bootstrap';
-import {Link} from "react-router-dom";
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
-export default class Nav extends React.Component {
-  render() {    
-    return (
-      <Navbar bg="#435058" expand="lg">
-        <Container>
-          <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link to="/">Home</Nav.Link>
-              <Nav.Link to="/market">Market</Nav.Link>
-              <Nav.Link to="/create">Create</Nav.Link>
-              <Nav.Link to="/about">About</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    );
-  }
+export default function NavBar() {   
+  return (
+    <Navbar bg="#435058" expand="lg">
+      <Container>
+        <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link to="/">Home</Nav.Link>
+            <Nav.Link to="/market">Market</Nav.Link>
+            <Nav.Link to="/create">Create</Nav.Link>
+            <Nav.Link to="/about">About</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
