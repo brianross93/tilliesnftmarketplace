b05f94d9f9fcbaa877ec1fe02659ae7774d24906
Danika H Jaskulak-Gonzalez <danikaherminise@gmail.com>
2022-01-06 18:04:26 -0800

    navbar edit

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -2,19 +2,34 @@
 import './NavBar.css';
 import React from "react";
 import { Navbar, Nav, Container } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 export default function NavBar() {   
   return (
-    <Navbar bg="#435058" expand="lg">
+    <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="/">
+        <img
+          src={"./TiLLiES_logo.png"}
+         
+          alt="Tillies logo"
+        />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link to="/">Home</Nav.Link>
-            <Nav.Link to="/market">Market</Nav.Link>
-            <Nav.Link to="/create">Create</Nav.Link>
-            <Nav.Link to="/about">About</Nav.Link>
+            <LinkContainer to="/">
+              <Nav.Link to="/">Home</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/market">
+              <Nav.Link to="/market">Market</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/create">
+              <Nav.Link to="/create">Create</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/about">
+              <Nav.Link to="/about">About</Nav.Link>
+            </LinkContainer>
           </Nav>
         </Navbar.Collapse>
       </Container>
