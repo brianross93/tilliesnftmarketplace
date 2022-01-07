44d74dc0dc02f6ea328ada6882b6d80de779ca11
Merissa Bridgeman <merissa.bridgeman98@gmail.com>
2022-01-06 21:19:48 -0800

    changed env

--- src/index.js
+++ src/index.js
@@ -5,27 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from 'firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseApp = initializeApp( {
-apiKey: process.env.REACT_APP_apiKey,
-authDomain: process.env.REACT_APP_authDomain,
-databaseURL: process.env.REACT_APP_databaseURL,
-projectId: process.env.REACT_APP_projectId,
-storageBucket: process.env.REACT_APP_storageBucket,
-messagingSenderId: process.env.REACT_APP_messagingSenderId,
-appId: process.env.REACT_APP_appId,
-});
-
-const analytics = getAnalytics(firebaseApp);
-const firestore = getFirestore()
 
 ReactDOM.render(
   <React.StrictMode>
