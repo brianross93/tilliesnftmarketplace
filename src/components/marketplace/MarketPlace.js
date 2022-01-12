21a48706c93da60b6154da90e3c8033505f84d7b
Ryan Lee <rjlguitar@gmail.com>
2022-01-12 13:01:28 -0800

    fixed import errors

--- src/components/marketplace/MarketPlace.js
+++ src/components/marketplace/MarketPlace.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import './MarketPlace.css';
-import Market from '../market/Market';
+import Market from './market/Market';
 
 function MarketPlace() {
-    return (
-      <div>
-          <h1>MarketPlace</h1>
-          <Market />
-      </div>
-        
-    )
+  return (
+    <div>
+      <h1>MarketPlace</h1>
+      <Market />
+    </div>
+  );
 }
 
 export default MarketPlace;
--- src/components/marketplace/market/Market.js
+++ src/components/marketplace/market/Market.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import '../../App.css';
-import Collection from '../../Collection';
-import SaleCollection from '../../SaleCollection';
+import '../../../App.css';
+import Collection from '../../../Collection';
+import SaleCollection from '../../../SaleCollection';
 import * as fcl from '@onflow/fcl';
 import { useState, useEffect } from 'react';
 
@@ -11,41 +11,35 @@ fcl
   .put('discovery.wallet', 'https://fcl-discovery.onflow.org/testnet/authn');
 
 function Market() {
-    const [address, setAddress] = useState();
-    const [officialAddress, setOfficialAddress] = useState('');
-    const [user, setUser] = useState();
+  const [address, setAddress] = useState();
+  const [officialAddress, setOfficialAddress] = useState('');
+  const [user, setUser] = useState();
 
-    useEffect(() => {
+  useEffect(() => {
     // sets the `user` variable to the person that is logged in through Blocto
     fcl.currentUser().subscribe(setUser);
   }, []);
 
-    console.log(<Collection address={officialAddress}></Collection>);
-    return (
-        <>
-        <div>
-            <h1>Search Page</h1>
-        </div>
-        <div>
-            <input type='text' onChange={(e) => setAddress(e.target.value)} />
-            <button onClick={() => setOfficialAddress(address)}>Search</button>
-        </div>
+  console.log(<Collection address={officialAddress}></Collection>);
+  return (
+    <>
+      <div>
+        <h1>Search Page</h1>
+      </div>
+      <div>
+        <input type='text' onChange={(e) => setAddress(e.target.value)} />
+        <button onClick={() => setOfficialAddress(address)}>Search</button>
+      </div>
 
-        { user && user.addr && officialAddress && officialAddress !== ''
-        ?
+      {user && user.addr && officialAddress && officialAddress !== '' ? (
         <Collection address={officialAddress}></Collection>
-        :
-        null
-      }
+      ) : null}
 
-      { user && user.addr && officialAddress && officialAddress !== ''
-      ?
-      <SaleCollection address={officialAddress}></SaleCollection>
-      :
-      null
-      }
-        </>
-    )
+      {user && user.addr && officialAddress && officialAddress !== '' ? (
+        <SaleCollection address={officialAddress}></SaleCollection>
+      ) : null}
+    </>
+  );
 }
 
 export default Market;
