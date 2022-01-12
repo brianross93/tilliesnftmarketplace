cfb589f69b735012a87c4de9f245eeb87fe5de0c
Ryan Lee <rjlguitar@gmail.com>
2022-01-12 13:47:23 -0800

    added base footer styles

---
+++ src/components/footer/footer.js
@@ -0,0 +1,12 @@
+import './footer.css';
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <p>© TiLLiES 2022</p>
+    </div>
+  );
+}
+
+export default Footer;
