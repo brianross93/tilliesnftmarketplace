011f0c21f8e26b1baf88ad935feb7afff8b03aba
Ryan Lee <rjlguitar@gmail.com>
2022-01-13 14:31:55 -0800

    added like and comment buttons to cards

--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -2,22 +2,32 @@
 import NFTs from './data.json';
 import './Cards.css';
 import { Card, Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart, faComments } from '@fortawesome/free-solid-svg-icons';
 
 export default function Cards() {
-  return(
-    <div className="Cards">
-      {NFTs.map(l => 
+  return (
+    <div className='Cards'>
+      {NFTs.map((l) => (
         <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src={process.env.PUBLIC_URL + '/images/' + l.image} alt="card" style={{ height: '18rem' }}/>
+          <Card.Img
+            variant='top'
+            src={process.env.PUBLIC_URL + '/images/' + l.image}
+            alt='card'
+            style={{ height: '18rem' }}
+          />
           <Card.Body>
             <Card.Title>{l.title}</Card.Title>
-            <Card.Text>
-              {l.price}
-            </Card.Text>
-            <Button variant="primary">favorite</Button>
+            <Card.Text>{l.price}</Card.Text>
+            <Button variant='primary'>
+              <FontAwesomeIcon icon={faHeart} />
+            </Button>
+            <Button variant='primary'>
+              <FontAwesomeIcon icon={faComments} />
+            </Button>
           </Card.Body>
         </Card>
-      )}
+      ))}
     </div>
-  )
+  );
 }
