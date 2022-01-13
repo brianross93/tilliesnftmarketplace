4a6dd3e751643cf10ff37d41c79a1a8cb197c4b5
Ryan Lee <rjlguitar@gmail.com>
2022-01-13 14:15:55 -0800

    adjusted hero styling

--- src/components/home/hero/Hero.js
+++ src/components/home/hero/Hero.js
@@ -18,7 +18,7 @@ export default function Hero() {
               </LinkContainer>
             </button>
           </Col>
-          <Col sm={4}>
+          <Col sm={4} className='image'>
             <img
               src='https://www.creativityculturecapital.org/wp-content/uploads/2021/09/22_Diego_Balan_Web-942x1024.png'
               alt='NFT'
