c5f8ed6d0be529d7573cb80842e61a437813b7a1
Danika H Jaskulak-Gonzalez <danikaherminise@gmail.com>
2022-01-13 13:26:37 -0800

    edited cards

--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -6,22 +6,18 @@ import { Card, Button } from 'react-bootstrap';
 export default function Cards() {
   return(
     <div className="Cards">
-      <ul className="NFTs">
-        {NFTs.map(l => 
-          <li className="card">
-            <Card style={{ width: '18rem' }}>
-              <Card.Img variant="top" src={process.env.PUBLIC_URL + '/images/' + l.image} alt="card" style={{ height: '18rem' }}/>
-              <Card.Body>
-                <Card.Title>{l.title}</Card.Title>
-                <Card.Text>
-                  {l.price}
-                </Card.Text>
-                <Button variant="primary">favorite</Button>
-              </Card.Body>
-            </Card>
-          </li>
-        )}
-      </ul>
+      {NFTs.map(l => 
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={process.env.PUBLIC_URL + '/images/' + l.image} alt="card" style={{ height: '18rem' }}/>
+          <Card.Body>
+            <Card.Title>{l.title}</Card.Title>
+            <Card.Text>
+              {l.price}
+            </Card.Text>
+            <Button variant="primary">favorite</Button>
+          </Card.Body>
+        </Card>
+      )}
     </div>
   )
 }
