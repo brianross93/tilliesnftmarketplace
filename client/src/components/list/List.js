e1c4a5b87a44792bbba4b1f52217e6892d785963
Merissa Bridgeman <merissa.bridgeman98@gmail.com>
2022-01-14 01:57:32 -0800

    fixed merge

--- client/src/components/list/List.js
+++ client/src/components/list/List.js
@@ -1,13 +1,25 @@
 import {useEffect, useState} from 'react';
 import './List.css';
 import Axios from 'axios';
+import * as fcl from '@onflow/fcl';
+import * as t from '@onflow/types';
+import { create } from 'ipfs-http-client';
+import { mintNFT } from '../../cadence/transactions/mint_nft.js';
 // import Mint from './Mint.js';
+
+const client = create('https://ipfs.infura.io:5001/api/v0');
+
+fcl
+  .config()
+  .put('accessNode.api', 'https://access-testnet.onflow.org')
+  .put('discovery.wallet', 'https://fcl-discovery.onflow.org/testnet/authn');
 
 function List() {
 
   const [name, setName] = useState('')
   const [id, setId] = useState(0)
   const [price, setPrice] = useState(0)
+  const [file, setFile] = useState()
   const [description, setDescription] = useState('')
   const [forSale, setForSale] = useState(false)
   
@@ -33,22 +45,11 @@ function List() {
     })
   }, [])
 
-  // const addNft = () => {
-  //   Axios.post('http://localhost:3001/insert', {
-  //     name: name,
-  //     id: id,
-  //     price: price,
-  //     description: description,
-  //     forSale: forSale
-  //   }, axiosConfig)
-  // }
-
   const addNft = () => {  
     Axios.post('http://localhost:3001/insert', {
       name: name,
       id: id,
       price: price,
-      description: description,
       forSale: forSale
     }, axiosConfig)
     .then(res => {
@@ -58,14 +59,10 @@ function List() {
       console.log(err)
     })
   }
-  
-
-  
-
 
   useEffect(() => {
     Axios.get('http://localhost:3001/insert', {
-      params: { name: name, id: id, price: price, description: description, forSale: forSale }  
+      params: { name: name, id: id, price: price, forSale: forSale }  
       //params: {name: name, id: id, price: price, description: description, forSale: forSale}
 }
 ).then(res => {
@@ -73,6 +70,30 @@ function List() {
 } )
     
 })
+
+const mint = async () => {
+
+    try {
+      const added = await client.add(file);
+      const hash = added.path;
+
+      const transactionId = await fcl
+        .send([
+          fcl.transaction(mintNFT),
+          fcl.args([fcl.arg(hash, t.String), fcl.arg(setName, t.String)]),
+          fcl.payer(fcl.authz),
+          fcl.proposer(fcl.authz),
+          fcl.authorizations([fcl.authz]),
+          fcl.limit(9999),
+        ])
+        .then(fcl.decode);
+
+      console.log(transactionId);
+      return fcl.tx(transactionId).onceSealed();
+    } catch (error) {
+      console.log('Error uploading file: ', error);
+    }
+  };
   return (
     <div className='list'>
       <label>Name:</label>
@@ -83,16 +104,13 @@ function List() {
       <input type='number' onChange={(e) => {
         setId(e.target.value)
       }} />
+      <label>Filename:</label>
+       <input type='file' onChange={(e) => setFile(e.target.files[0])} />
       <label>price:</label>
       <input type='number' onChange={(e) => {
         setPrice(e.target.value)
       }} />
-      <label>description:</label>
-      <input type='text' onChange={(e) => {
-        setDescription(e.target.value)
-        setForSale()
-      }} />
-      <button onClick={addNft}>List NFT</button>
+      <button onClick={() => {addNft(); mint();}}>List NFT</button>
     </div>
   );
 }
--- client/src/components/list/Mint.js
+++ client/src/components/list/Mint.js
@@ -5,6 +5,7 @@ import * as fcl from '@onflow/fcl';
 import * as t from '@onflow/types';
 import { useState, useEffect } from 'react';
 import { create } from 'ipfs-http-client';
+import './List.css';
 import { mintNFT } from '../../cadence/transactions/mint_nft.js';
 import { listForSaleTx } from '../../cadence/transactions/list_for_sale.js';
 import { unlistFromSaleTx } from '../../cadence/transactions/unlist_from_sale.js';
@@ -18,21 +19,67 @@ fcl
 
 function Mint() {
   const [user, setUser] = useState();
-  const [nameOfNFT, setNameOfNFT] = useState('');
-  const [file, setFile] = useState();
+  const [name, setName] = useState();
+  // const [nameOfNFT, setNameOfNFT] = useState('');
   const [id, setID] = useState();
   const [price, setPrice] = useState();
   const [officialAddress, setOfficialAddress] = useState('');
+  const [forSale, setForSale] = useState(false)
 
-  useEffect(() => {
-    // sets the `user` variable to the person that is logged in through Blocto
-    fcl.currentUser().subscribe(setUser);
-  }, []);
+//   useEffect(() => {
+//     // sets the `user` variable to the person that is logged in through Blocto
+//     fcl.currentUser().subscribe(setUser);
+//   }, []);
 
-  // This is an async function for minting NFTs. It's calling mintNFT imported from mint_nft.js
+//   let axiosConfig = {
+//     headers: {
+//         'Content-Type': 'application/json;charset=UTF-8',
+//         "Access-Control-Allow-Origin": "*",
+//     }
+//   };
+
+//   useEffect(() => {
+//     Axios.get('http://localhost:3001/read', axiosConfig)
+//     .then(res => {
+//       console.log(res.data)
+//       setName(res.data[0].name)
+//       setFile(res.data[0].file)
+//       setID(res.data[0].id)
+//       setPrice(res.data[0].price)
+//       setForSale(res.data[0].forSale)
+//     })
+//     .catch(err => {
+//       console.log(err)
+//     })
+//   }, [])
+
+//   const mintNft = () => {  
+//     Axios.post('http://localhost:3001/insert', {
+//       name: name,
+//       file: file
+//     }, axiosConfig)
+//     .then(res => {
+//       console.log(res)
+//     })
+//     .catch(err => {
+//       console.log(err)
+//     })
+//   }
+
+//   useEffect(() => {
+//     Axios.get('http://localhost:3001/insert', {
+//       params: { name: name, id: id, price: price, forSale: forSale }  
+//       //params: {name: name, id: id, price: price, description: description, forSale: forSale}
+// }
+// ).then(res => {
+//     console.log(res)
+// } )
+
+// })
 
   //most of these are ultimately passing arguents to functions that have been imported
   const mint = async () => {
+
     try {
       const added = await client.add(file);
       const hash = added.path;
@@ -40,7 +87,7 @@ function Mint() {
       const transactionId = await fcl
         .send([
           fcl.transaction(mintNFT),
-          fcl.args([fcl.arg(hash, t.String), fcl.arg(nameOfNFT, t.String)]),
+          fcl.args([fcl.arg(hash, t.String), fcl.arg(name, t.String)]),
           fcl.payer(fcl.authz),
           fcl.proposer(fcl.authz),
           fcl.authorizations([fcl.authz]),
@@ -57,6 +104,18 @@ function Mint() {
 
   // Sends argjuents for listForSaleTx
   const listForSale = async () => {
+    // //post to our server route
+    // Axios.post('http://localhost:3001/insert', {
+    //   price: price,
+    //   forSale: forSale
+    // }, axiosConfig)
+    // .then(res => {
+    //   console.log(res)
+    // })
+    // .catch(err => {
+    //   console.log(err)
+    // })
+    setForSale(true)
     const transactionId = await fcl
       .send([
         fcl.transaction(listForSaleTx),
@@ -82,6 +141,7 @@ function Mint() {
         fcl.authorizations([fcl.authz]),
         fcl.limit(9999),
       ])
+      setForSale(false)
       .then(fcl.decode);
 
     console.log(transactionId);
@@ -91,20 +151,18 @@ function Mint() {
   return (
     <div className='mint'>
       <div>
-        <input type='text' onChange={(e) => setNameOfNFT(e.target.value)} />
+        <input type='text' onChange={(e) => setName(e.target.value)} />
         <input type='file' onChange={(e) => setFile(e.target.files[0])} />
         <button onClick={() => mint()}>Mint</button>
       </div>
 
       <div>
-        <form action='/list' method='post'>
           <input type='text' onChange={(e) => setID(e.target.value)} />
           <input type='text' onChange={(e) => setPrice(e.target.value)} />
           <button onClick={() => listForSale()}>Lift NFT for Sale</button>
           <button onClick={() => unlistFromSale()}>
             Unlist an NFT from Sale
           </button>
-        </form>
       </div>
 
       {/* This ultimately shows the collection  */}
