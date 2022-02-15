a9771966db9b54201bebf08d8e7d41a6d61b1217
brianross93 <brssnj45@gmail.com>
2022-02-14 19:31:50 -0500

    changing database models for comments

--- server/models/comments.js
+++ server/models/comments.js
@@ -1,13 +1,40 @@
 import * as mongoose from "mongoose";
-const Schema = mongoose.Schema;
+// const Schema = mongoose.Schema;
 
-const CommentSchema = new Schema({
-  content: { type: String, required: true },
-  comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }]
-},
-  {timestamps: {createdAt: 'created_at'}},
+// const CommentSchema = new Schema({
+//   content: { type: String, required: true },
+//   comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }]
+// },
+//   {timestamps: {createdAt: 'created_at'}},
   
-);
+// );
 
+// Create a new schema to hold comments for the NFT 
+const commentSchema = new mongoose.Schema({
+  comment: String,
+  user: String,
+  date: Date
+});
+// Create a new schema to hold NFTs
+const nftSchema = new mongoose.Schema({
+  name: String,
+  id: String,
+  price: Number,
+  description: String,
+  forSale: Boolean,
+  comments: [commentSchema]
+});
+// Create a new schema to hold the list of NFTs
+const listSchema = new mongoose.Schema({
+  nfts: [nftSchema]
+});
+
+// Export the list model
+//# sourceMappingURL=listnft.js.map
+
+
+
+
+module.exports = mongoose.model("Comment", CommentSchema);
 
 module.exports = mongoose.model("Comment", CommentSchema);
--- server/models/mintModel.js
+++ server/models/mintModel.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose");
-
-const MintNftSchema = new mongoose.Schema({
-  name: {
-      type: String,
-      required: true
-  },
-  file: {
-    type: String,
-    required: false,
-  }
+// Create a databse model for when an NFT is minted. When an NFT is minted, it is added to the database.
+const mintSchema = new mongoose.Schema({
+  name: String,
+  id: String,
+  price: Number,
+  description: String,
+  forSale: Boolean
 });
 
-module.exports = mongoose.model("mint", MintNftSchema);
+
+
+
+
+module.exports = mongoose.model("mint", mintSchema);
